84577752c9376d01a813b1306ef6c4686fb3c3ef
Josh F <josh@vector35.com>
2023-01-05 17:26:01 -0500

    Wait for database to be ready before running migrations

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,24 @@
 #!/bin/bash
 
+
+health_check () {
+    pipenv run ./manage.py check --database default;
+    return $?;
+}
+
+HEALTH_CHECK_INTERVAL=5;
+HEALTH_CHECK_ATTEMPTS=0;
+HEALTH_CHECK_ATTEMPT_LIMIT=5;
+while ! health_check; do
+    HEALTH_CHECK_ATTEMPTS=$((HEALTH_CHECK_ATTEMPTS + 1));
+    echo "Health check attempt ${HEALTH_CHECK_ATTEMPTS} failed, retrying in ${HEALTH_CHECK_INTERVAL} seconds...";
+    if [ "$HEALTH_CHECK_ATTEMPTS" -ge "$HEALTH_CHECK_ATTEMPT_LIMIT" ]; then
+        echo "Exceeded number of health check attempts, exiting";
+        exit 1;
+    fi;
+    sleep $HEALTH_CHECK_INTERVAL;
+done
+
 set -e
 
 pipenv run ./manage.py migrate -v 0
