47177a358fd33d922429446bd9b79bdd188ac491
Kevin Phoenix <kevin@kphoenix.us>
2023-02-06 17:59:52 -0700

    Set git user.name for angr update commit

--- .github/workflows/update-angr.sh
+++ .github/workflows/update-angr.sh
@@ -5,6 +5,7 @@ sed -i "s/^RUN pip install --user 'angr.*$/RUN pip install --user 'angr~=$ANGR_V
 if [ ! -z "$(git diff --name-only)" ]; then
     git checkout -b angr-$ANGR_VERSION
     git add runners/decompiler/Dockerfile
+    git config --global user.name "Github Actions"
     git commit -m "Update angr to $ANGR_VERSION"
     git push origin angr-$ANGR_VERSION
     gh pr create --fill --assignee twizmwazin
