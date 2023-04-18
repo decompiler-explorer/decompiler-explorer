ff40a96fe9c61682bef05ba10b6cdc3e307e0d2c
Josh Ferrell <josh@vector35.com>
2023-04-17 21:35:18 -0400

    Sliding window throttle

--- decompiler_explorer/throttle.py
+++ decompiler_explorer/throttle.py
@@ -1,7 +1,16 @@
 from rest_framework.throttling import AnonRateThrottle
 
-class AnonBurstRateThrottle(AnonRateThrottle):
+
+class AnonRateThrottleSliding(AnonRateThrottle):
+    def throttle_failure(self):
+        if len(self.history) >= self.num_requests: # type: ignore
+            self.history.pop(-1)
+        self.history.insert(0, self.now)
+        self.cache.set(self.key, self.history, self.duration)
+        return super().throttle_failure()
+
+class AnonBurstRateThrottle(AnonRateThrottleSliding):
     scope = 'anon_burst'
 
-class AnonSustainedRateThrottle(AnonRateThrottle):
+class AnonSustainedRateThrottle(AnonRateThrottleSliding):
     scope = 'anon_sustained'
