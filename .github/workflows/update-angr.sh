3b82cc565fe5ccd933f76185ded01151242765ed
Kevin Phoenix <kevin@kphoenix.us>
2024-04-19 15:27:12 -0700

    Update github actions git config (#160)

--- .github/workflows/update-angr.sh
+++ .github/workflows/update-angr.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
 
+git config user.name "github-actions[bot]"
+git config user.email "41898282+github-actions[bot]@users.noreply.github.com"
+
 ANGR_VERSION=$(python -c "import feedparser; print(feedparser.parse('https://pypi.org/rss/project/angr/releases.xml').entries[0].title)")
 sed -i "s/^RUN pip install --user 'angr.*$/RUN pip install --user 'angr~=$ANGR_VERSION'/g" runners/decompiler/Dockerfile
 if [ ! -z "$(git diff --name-only)" ]; then
     git checkout -b angr-$ANGR_VERSION
     git add runners/decompiler/Dockerfile
-    git config --global user.name "Github Actions"
     git commit -m "Update angr to $ANGR_VERSION"
     git push origin angr-$ANGR_VERSION
     gh pr create --fill --assignee twizmwazin
